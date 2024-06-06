ae3010094ed704ee81a3da5f61da6448937e8ff5
gui777x <guizin.clementino@gmail.com>
2024-06-06 12:17:26 -0300

    Terminado

--- EtecShop/Data/AppDbContext.cs
+++ EtecShop/Data/AppDbContext.cs
@@ -5,16 +5,16 @@ using Microsoft.EntityFrameworkCore;
 namespace EtecShop.Data;
 public class AppDbContext : IdentityDbContext
 {
-public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-{ }
-public DbSet<Avaliacao> Avaliacoes { get; set; }
-public DbSet<Categoria> Categorias { get; set; }
-public DbSet<Produto> Produtos { get; set; }
-protected override void OnModelCreating(ModelBuilder builder)
-{
-base.OnModelCreating(builder);
-#region Populando os dados da Gestão de Usuários
-List<IdentityRole> roles = new()
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    { }
+    public DbSet<Avaliacao> Avaliacoes { get; set; }
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        #region Populando os dados da Gestão de Usuários
+        List<IdentityRole> roles = new()
 {
 new IdentityRole(){
 Id = Guid.NewGuid().ToString(),
@@ -34,21 +34,21 @@ Name = "Cliente",
 NormalizedName = "CLIENTE"
 }
 };
-builder.Entity<IdentityRole>().HasData(roles);
-IdentityUser user = new()
-{
-Id = Guid.NewGuid().ToString(),
-Email = "admin@etecshop.com",
-NormalizedEmail = "ADMIN@ETECSHOP.COM",
-UserName = "Admin",
-NormalizedUserName = "ADMIN",
-LockoutEnabled = true,
-EmailConfirmed = true,
-};
-PasswordHasher<IdentityUser> pass = new();
-user.PasswordHash = pass.HashPassword(user, "@Etec123");
-builder.Entity<IdentityUser>().HasData(user);
-List<IdentityUserRole<string>> userRoles = new()
+        builder.Entity<IdentityRole>().HasData(roles);
+        IdentityUser user = new()
+        {
+            Id = Guid.NewGuid().ToString(),
+            Email = "admin@etecshop.com",
+            NormalizedEmail = "ADMIN@ETECSHOP.COM",
+            UserName = "Admin",
+            NormalizedUserName = "ADMIN",
+            LockoutEnabled = true,
+            EmailConfirmed = true,
+        };
+        PasswordHasher<IdentityUser> pass = new();
+        user.PasswordHash = pass.HashPassword(user, "@Etec123");
+        builder.Entity<IdentityUser>().HasData(user);
+        List<IdentityUserRole<string>> userRoles = new()
 {
 new IdentityUserRole<string>() {
 UserId = user.Id,
@@ -62,11 +62,12 @@ new IdentityUserRole<string>() {
 UserId = user.Id,
 RoleId = roles[2].Id
 }};
-builder.Entity<IdentityUserRole<string>>().HasData(userRoles);
-#endregion
+        builder.Entity<IdentityUserRole<string>>().HasData(userRoles);
+        #endregion
 
-#region Popular Categorias
-List<Categoria> categorias = new(){
+
+        #region Popular Categorias
+        List<Categoria> categorias = new(){
 new Categoria() {
 Id = 1,
 Nome = "Livros e papelaria"
@@ -96,45 +97,11 @@ Id = 7,
 Nome = "Móveis e Decoração"
 }
 };
-builder.Entity<Categoria>().HasData(categorias);
-#endregion
+        builder.Entity<Categoria>().HasData(categorias);
+        #endregion
 
-#region Popular Categorias
-List<Categoria> Categorias = new(){
-new Categoria() {
-Id = 1,
-Nome = "Livros e papelaria"
-},
-new Categoria() {
-Id = 2,
-Nome = "Games e PC Gamer"
-},
-new Categoria() {
-Id = 3,
-Nome = "Informática"
-},
-new Categoria() {
-Id = 4,
-Nome = "Smartphones"
-},
-new Categoria() {
-Id = 5,
-Nome = "Eletrodomesticos e Casa"
-},
-new Categoria() {
-Id = 6,
-Nome = "Beleza e Perfumaria"
-},
-new Categoria() {
-Id = 7,
-Nome = "Móveis e Decoração"
-}
-};
-builder.Entity<Categoria>().HasData(categorias);
-#endregion
-
-#region Popular Produtos
-List<Produto> produtos = new()
+        #region Popular Produtos
+        List<Produto> produtos = new()
 {
 new Produto(){
 Id = 1,
@@ -208,7 +175,7 @@ CategoriaId = 5,
 Foto = "/img/produtos/8.png"
 }
 };
-builder.Entity<Produto>().HasData(produtos);
-#endregion
-}
+        builder.Entity<Produto>().HasData(produtos);
+        #endregion
+    }
 }
